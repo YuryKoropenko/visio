e207539c1a0430c517819b45301e2e45ee9807f8
Yury Koropenko <yurykoropenko@gmail.com>
2016-12-29 11:22:23 +0200

    commit 2

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+
+/**/
+	$('.mob-menu__icon').on('click', function() {
+		$(this).parent().children('.nav-toggle').stop(true, false).fadeToggle(300);
+		$(document).click(function(event) {
+			if ($(event.target).closest(".nav-toggle").length) return;
+				$(".nav-toggle").fadeOut(300);
+				event.stopPropagation();
+			});
+		return false
+	});
+
+});
