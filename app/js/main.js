b15a0b91e81bf37031784c7d252b85fd8c0fb394
Yury Koropenko <yurykoropenko@gmail.com>
2017-01-03 14:13:19 +0200

    commit 4

--- app/js/main.js
+++ app/js/main.js
@@ -12,9 +12,32 @@ $(document).ready(function() {
 	});
 
 /**/
-	$('.sidebar-menu__title').on('click', function() {
+	$('.sidebar .sidebar-menu__title').on('click', function() {
+		$('.sidebar-menu__wrapper').slideUp(300);
 		$(this).parent().children('.sidebar-menu__wrapper').stop(true, false).slideToggle(300);
 		$(this).children('.sidebar-menu__title-name').toggleClass('sidebar-menu__title-name-open');
+	});
+
+/**/
+	$('.sidebar-toggle-menu .sidebar-menu__title').on('click', function() {
+		$('.sidebar-menu__wrapper').fadeOut(300);
+		$('.sidebar-menu__title').removeClass('sidebar-menu__title-active');
+		$(this).parent().children('.sidebar-menu__wrapper').stop(true, false).fadeToggle(300);
+		$(this).toggleClass('sidebar-menu__title-active');
+	});
+
+/**/
+	$('.menu-sidebar').on('click', function() {
+		$('.sidebar-toggle-menu').fadeOut(300);
+		$('.menu-sidebar').removeClass('menu-sidebar-active');
+		$(this).parent().children('.sidebar-toggle-menu').stop(true, false).slideToggle(300);
+		$(this).toggleClass('menu-sidebar-active');
+	});
+
+/**/
+	$('.sidebar-line-mob .sidebar-menu__title').on('click', function() {
+		$('.sidebar-menu__title').hide();
+		
 	});
 
 });
